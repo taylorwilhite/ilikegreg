d2cc95d5555f55dbf058955ce3d094bcf6aa65bf
Taylor Wilhite <taylorwilhite@gmail.com>
2021-02-11 10:04:38 -0800

    add greg liking

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { 
+  ChakraProvider,
+  Input,
+  Flex,
+  Box,
+  Button,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Image
+} from '@chakra-ui/react';
+import logo from './logo.jpg';
 
 function App() {
+  const {isOpen, onOpen, onClose } = useDisclosure()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Box w="100%">
+        <Flex w="100%" h="100vh" justify="center" align="center" wrap="wrap" flexDir="column">
+            <Image src={logo} w="20%" />
+            <Input m={2} w="55%" placeholder="social security number" size="md"/>
+            <Input m={2} w="55%" placeholder="password" type="password" size="md"/>
+          <Button onClick={onOpen} colorScheme="blue" size="lg">
+            I now and forever like Greg; see you tomorrow
+          </Button>
+
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Thanks for liking Greg!</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                Come back tomorrow to keep liking Greg!
+              </ModalBody>
+
+              <ModalFooter>
+                <Button colorScheme="blue" mr={3} onClick={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+            
+        </Flex>
+      </Box>
+    </ChakraProvider>
   );
 }
 
